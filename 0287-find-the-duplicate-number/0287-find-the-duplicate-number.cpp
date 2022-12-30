bfe2e3cbc247ecd622a22c3fd30f3a9fef73c6be
KNVS Sai Sneha <snehasai01@gmail.com>
2022-12-30 19:44:02 +0530

    Time: 115 ms (92.20%), Space: 61.3 MB (30.44%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        //cycle detection linked list
+       int s = nums[0], f = nums[0];
+       
+       do{
+           s  = nums[s];
+           f = nums[nums[f]];
+       }while(s != f);
+        
+       f = nums[0];
+       
+       while(s != f)
+       {
+           s = nums[s];
+           f = nums[f];
+       }
+        
+        return s;
+    
+        
+    }
+};
